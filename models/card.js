4c415f2dceace816d87e10039da55825167ec9ed
AKatryukhin <A.Katryukhin@yandex.ru>
2021-06-15 12:41:47 +0300

    feat: added cardSchems

---
+++ models/card.js
@@ -0,0 +1,28 @@
+const { ObjectID } = require('bson');
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  owner: {
+    type: ObjectID,
+    required: true,
+  },
+  likes: [{
+    default: []
+  }],
+  link: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('card', cardSchema);
