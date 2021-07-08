d751db8cc758b8af81df8f6afcd380be4061123a
AKatryukhin <A.Katryukhin@yandex.ru>
2021-07-08 16:54:56 +0300

    feat: added Access-Control-Allow-Origin *

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ app.use((req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    // res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
