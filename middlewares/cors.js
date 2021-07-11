86614320490f439be2516300c355d13174446ff0
AKatryukhin <A.Katryukhin@yandex.ru>
2021-07-11 14:24:20 +0300

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -9,11 +9,8 @@ module.exports = ((req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header(
-      'Access-Control-Allow-Origin', origin,
-      'Access-Control-Allow-Credentials', true,
-    );
-    // res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
